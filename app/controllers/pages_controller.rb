d28548673539de19b20022c58f60bc4294c87a1d
tino calancha <tino.calancha@gmail.com>
2018-02-19 12:52:55 +0900

    Support few more questions

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,5 +4,13 @@ class PagesController < ApplicationController
 
   def answer
     @question = params[:question]
+    case @question.downcase
+    when 'hello'
+      @answer = 'hello'
+    when 'what time is it?'
+      @answer = 'The same time that yesterday at the same moment.'
+    else
+      @answer = "I don't know it"
+    end
   end
 end
